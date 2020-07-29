d0dcfbf8f9be8e505c9699a98d1642c362ca7a3e
Rich Trouton <rtrouton@yahoo.com>
2020-07-29 15:43:31 -0400

    Update OfficeSuiteSKULessVersionProvider.py

--- MicrosoftOfficeSuite365/OfficeSuiteSKULessVersionProvider.py
+++ MicrosoftOfficeSuite365/OfficeSuiteSKULessVersionProvider.py
@@ -14,18 +14,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import urllib2
+from __future__ import absolute_import
+
 import xml.etree.ElementTree as ET
 
-from autopkglib import Processor, ProcessorError
-
+from autopkglib import Processor, ProcessorError, URLGetter
 
 __all__ = ["OfficeSuiteSKULessVersionProvider"]
 
 FEED_URL = "https://macadmins.software/latest.xml"
 
-class OfficeSuiteSKULessVersionProvider(Processor):
+
+class OfficeSuiteSKULessVersionProvider(URLGetter):
     """Provides the version of the latest SKU-Less Office 2016 Suite release"""
+
     input_variables = {}
     output_variables = {
         "version": {
@@ -33,21 +35,20 @@ class OfficeSuiteSKULessVersionProvider(Processor):
         },
     }
     description = __doc__
-    
+
     def get_version(self, FEED_URL):
         """Parse the macadmins.software/versions.xml feed for the latest O365 version number"""
         try:
-            raw_xml = urllib2.urlopen(FEED_URL)
-            xml = raw_xml.read()
-        except BaseException as e:
+            xml = self.download(FEED_URL)
+        except Exception as e:
             raise ProcessorError("Can't download %s: %s" % (FEED_URL, e))
-        
+
         root = ET.fromstring(xml)
-        latest = root.find('latest')
-        for vers in root.iter('latest'):
-            version = vers.find('o365').text
+        latest = root.find("latest")
+        for vers in root.iter("latest"):
+            version = vers.find("o365").text
         return version
-    
+
     def main(self):
         self.env["version"] = self.get_version(FEED_URL)
         self.output("Found Version Number %s" % self.env["version"])
